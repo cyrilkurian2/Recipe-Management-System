518d0b4f243610fb4f20fe8fcdc7a024b1f16514
cyrilkurian2 <cyril.kurian@geekywolf.local>
2024-10-30 12:44:55 +0530

    added homepage

--- recipe-manage/src/app/app.component.ts
+++ recipe-manage/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from "./user/user.component";
+import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent],
+  imports: [RouterOutlet, UserComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'login-page';
+  title = 'recipe-manage';
 }
--- recipe-manage/src/app/app.routes.ts
+++ recipe-manage/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+    { path:'user-signup', component: UserComponent},
+    { path:'home-page', component: HomepageComponent }
+];
