23084267aa24e5665603acc2d4910177811632ff
cyrilkurian2 <cyril.kurian@geekywolf.local>
2024-10-30 16:19:25 +0530

    profile page added

--- recipe-manage/src/app/app.component.ts
+++ recipe-manage/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from "./user/user.component";
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, HomepageComponent],
+  imports: [RouterOutlet, UserComponent, HomepageComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- recipe-manage/src/app/app.routes.ts
+++ recipe-manage/src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 
 
 
 
 export const routes: Routes = [
-    { path:'user-signup', component: UserComponent},
-    { path:'home-page', component: HomepageComponent }
+    // { path:'user', component: UserComponent},
+    { path:'home-page', component: HomepageComponent },
+    { path:'profile', component: ProfileComponent},
+
+    { 
+        path: 'user', 
+        component: UserComponent,
+        children: [
+            // { path: 'login', component: LoginComponent },
+            { path: 'signup', component: RegistrationComponent }
+        ]
+    },
 ];
