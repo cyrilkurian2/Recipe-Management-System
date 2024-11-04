75a1bdfb1cab918ab79ef42a6c3d0ff19df9d4d1
cyrilkurian2 <cyril.kurian@geekywolf.local>
2024-11-04 19:48:57 +0530

    getfav query , getprofileRecipequery, delete recipe by id

---
+++ RecipeManagement/RecipeManagement.Application/Requets/Querries/DeleteRecipeByIdQuery.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RecipeManagement.Application.DTO;
+using RecipeManagement.Infrastructure.Data;
+
+namespace RecipeManagement.Application.Requets.Querries
+{
+
+    public class DeleteRecipeByIdQuery : IRequest<bool>
+    {
+        public int RecipeId { get; set; }
+
+        public DeleteRecipeByIdQuery(int recipeId)
+        {
+            RecipeId = recipeId;
+        }
+    }
+
+    public class DeleteRecipeByIdQueryHandler : IRequestHandler<DeleteRecipeByIdQuery, bool>
+    {
+        private readonly RecipeManagementContext _context;
+
+        public DeleteRecipeByIdQueryHandler(RecipeManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(DeleteRecipeByIdQuery request, CancellationToken cancellationToken)
+        {
+            // Retrieve the recipe with related Favourites
+            var recipe = await _context.Recipes
+                 // Ensure Favourites are included for deletion
+                .FirstOrDefaultAsync(r => r.RecipeId == request.RecipeId, cancellationToken);
+
+            if (recipe == null)
+            {
+                return false; // Recipe not found, handle in controller
+            }
+
+            // Delete associated favourites
+            var favouritesToDelete = await _context.Favourites
+                .Where(f => f.recipe.RecipeId == request.RecipeId)
+                .ToListAsync(cancellationToken);
+
+            _context.Favourites.RemoveRange(favouritesToDelete);
+
+            // Delete the recipe
+            _context.Recipes.Remove(recipe);
+
+            // Save all changes to the database
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true; // Indicate successful deletion
+        }
+
+    }
+}
--- RecipeManagement/RecipeManagement.Application/Requets/Querries/GetProfileRecipeQuery.cs
+++ RecipeManagement/RecipeManagement.Application/Requets/Querries/GetProfileRecipeQuery.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using RecipeManagement.Application.DTO;
 using RecipeManagement.Infrastructure.Data;
 
@@ -14,7 +15,7 @@ namespace RecipeManagement.Application.Requets.Querries
         public int UserId { get; set; }
     }
 
- 
+
 
     public class GetProfileRecipeQueryHandler : IRequestHandler<GetProfileRecipeQuery, List<RecipeDTO>>
     {
@@ -25,28 +26,62 @@ namespace RecipeManagement.Application.Requets.Querries
             _context = context;
         }
 
-        //public async Task<List<RecipeDTO>> Handle(GetProfileRecipeQuery request, CancellationToken cancellationToken)
-        //{
-        //    var recipes = await _context.Recipes
-        //        .Include(r => r.category)
-        //        .Where(x => x.UserId == request.UserId)
-        //        //.Where()
-        //        .Select(recipe => new RecipeDTO 
-        //        {
-        //            RecipeId = recipe.RecipeId,
-        //            RecipeTitle = recipe.RecipeTitle,
-        //            RecipeDescription = recipe.RecipeDescription,
-        //            Duration = recipe.Duration,
-        //            IsComplete = recipe.IsComplete,
-        //            categoryDTO = new CategoryDTO
-        //            {
-        //                CategoryId = recipe.category.CategoryId,
-        //                CategoryName = recipe.category.CategoryName 
-        //            }
-        //        })
-        //    .ToListAsync(cancellationToken);
+        public async Task<List<RecipeDTO>> Handle(GetProfileRecipeQuery request, CancellationToken cancellationToken)
+        {
+            //var recipes = await _context.RecipeAuthors
+            //.Where(ra => ra.User.UserId == request.UserId) // Filter by UserId
+            //.Select(ra => ra.Recipe) // Select associated Recipe
+            //.Select(recipe => new RecipeDTO
+            //{
+            //    RecipeId = recipe.RecipeId,
+            //    RecipeTitle = recipe.RecipeTitle,
+            //    RecipeDescription = recipe.RecipeDescription,
+            //    Duration = recipe.Duration,
+            //    IsComplete = recipe.IsComplete,
+            //    categoryDTO = new CategoryDTO
+            //    {
+            //        CategoryId = recipe.category.CategoryId,
+            //        CategoryName = recipe.category.CategoryName
+            //    }
+            //})
+            //.ToListAsync(cancellationToken);
 
-        //    return recipes;
-        //}
+            //return recipes;
+
+
+            var recipes = await _context.RecipeAuthors
+            .Where(ra => ra.User.UserId == request.UserId) // Filter by UserId
+            .Select(ra => ra.Recipe) // Select associated Recipe
+            .GroupJoin(
+                _context.Favourites,
+                recipe => recipe.RecipeId,
+                favourite => favourite.recipe.RecipeId,
+                (recipe, favourites) => new { Recipe = recipe, FavouritesCount = favourites.Count() }
+            )
+            .Select(r => new RecipeDTO
+            {
+                RecipeId = r.Recipe.RecipeId,
+                RecipeTitle = r.Recipe.RecipeTitle,
+                RecipeDescription = r.Recipe.RecipeDescription,
+                Duration = r.Recipe.Duration,
+                IsComplete = r.Recipe.IsComplete,
+                categoryDTO = new CategoryDTO
+                {
+                    CategoryId = r.Recipe.category.CategoryId,
+                    CategoryName = r.Recipe.category.CategoryName
+                },
+                FavouritesCount = r.FavouritesCount // Include the count of users who favorited this recipe
+            })
+            .ToListAsync(cancellationToken);
+
+            return recipes;
+
+
+
+
+
+        }
+
+
     }
 }
