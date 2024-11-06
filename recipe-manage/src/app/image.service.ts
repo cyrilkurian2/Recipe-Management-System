07304f9cb4b8f85386ed37c8bb0ad2c00ee60bf1
Nohan Antony <nohan.antony@geekywolf.com>
2024-11-06 21:08:42 +0530

    to merge

---
+++ recipe-manage/src/app/image.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ImageService {
+  convertImageToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = (error) => {
+        reject(error);
+      };
+      reader.readAsDataURL(file);
+    });
+  }
+}
